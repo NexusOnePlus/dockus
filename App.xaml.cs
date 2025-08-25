5cd53bcc1c03d2246901409072afb1adc52e1554
NexusOnePlus <wv35788@gmail.com>
2025-08-24 20:20:16 -0500

    feat: setup installer

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using Velopack;
 
 namespace dockus;
 
@@ -7,5 +8,31 @@ namespace dockus;
 /// </summary>
 public partial class App : Application
 {
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        bool createdNew;
+        using (var mutex = new System.Threading.Mutex(true, "IconizerAppMutex", out createdNew))
+        {
+            if (!createdNew)
+            {
+                MessageBox.Show("App already running.", "Unique instance", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            try
+            {
+                VelopackApp.Build().Run();
+
+                var app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Fatal error on startup runtime: {ex.Message}", "Start failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
 }
 
