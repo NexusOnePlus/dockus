e27b062a20e4dac84d28b85803eb736a34f72849
NexusOnePlus <wv35788@gmail.com>
2025-08-25 19:34:08 -0500

    ui: window setting

--- SettingsApp/SettingsWindow.xaml.cs
+++ SettingsApp/SettingsWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace dockus.Settings
 {
@@ -7,9 +9,26 @@ namespace dockus.Settings
     /// </summary>
     public partial class SettingsWindow : Window
     {
+
+
+        [DllImport("dwmapi.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+
+        private const int DWMWA_USE_IMMERSIVE_DARK_MODE = 20;
+
+
+
         public SettingsWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var hwnd = new WindowInteropHelper(this).Handle;
+            int useDark = 1;
+            DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, ref useDark, sizeof(int));
         }
     }
 }
